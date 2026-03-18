bddeaf5d1b293c4bcd412463a81dc0937a4347bb
elisheva <elishevaronstam@gmail.com>
2026-03-18 02:13:59 +0200

    add back backend.env and removed sensitive data

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: process.env.VITE_SERVER_URL || "http://localhost:8000",
+  schema: process.env.VITE_SERVER_URL || "http://localhost:8000/graphql",
   documents: ["./src/graphql/*.ts"],
   generates: {
     "./src/graphql/generated/types.ts": {
